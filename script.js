407114075bdf064ed779ad4cf3ce77b40bdaf2e7
Myster1o619 <30766852+Myster1o619@users.noreply.github.com>
2024-05-14 11:00:15 +0200

    Update script.js
    Add const ROCK and value
    Add const PAPER and value
    Add const SCISSORS and value
    
    Add assignComputerChoice function
    - this function takes a number between 0 and 2 and assigns a choice for the pc based on this number
    - pc's choice becomes: "rock", "paper", or "scissors"

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 const totalRounds = 5;
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
 let playerScore = 0;
 let pcScore = 0;
 let playerChoice = "";
 let pcChoice = "";
 
 const initGame = () => {
-    pcChoice = generateComputerChoice();    
+    pcChoice = generateComputerChoice();
 }
 
 const generateComputerChoice = () => {
     //  Returns a random integer from 0 to 2:
     const chosenNumber = Math.floor(Math.random() * 3);
     // Assign number to an element for the pc:
-    assignComputerChoice(chosenNumber);
+    pcChoice = assignComputerChoice(chosenNumber);
+}
+
+// Take number between 0 - 2 (randomNum) and assign choice variable a value: rock/paper/scissors
+const assignComputerChoice = (randomNum = 0) => {
+    let choice = "";
+
+    switch (randomNum) {
+        case 0:
+            choice = ROCK;
+            break;
+
+        case 1:
+            choice = PAPER;
+            break;
+
+        case 2:
+            choice = SCISSORS;
+            break;
+    }
+    return choice;
 }
